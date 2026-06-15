d231b5d13f68d39a7eb86ff022e70b6a652645e3
federicosimone <federico.simone15@gmail.com>
2026-06-15 23:31:34 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,1 @@
+import type { Person } from "./typeAlias";
---
+++ src/typeAlias.ts
@@ -0,0 +1,14 @@
+export type Person = {
+    readonly id: number,
+    readonly name: string,
+    birth_year: number,
+    death_year?: number,
+    biography: string,
+    image: string
+}
+
+export type Actress = Person & {
+    most_famous_movies: [string, string, string],
+    awards: string,
+    nationality: "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese"
+}
